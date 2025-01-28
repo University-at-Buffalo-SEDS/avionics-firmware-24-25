2f5a4b6fca3b25263be17698f78d945869e3e113
Orangeturtles2 <zyadzahra06@gmail.com>
2025-01-28 18:17:36 -0500

    Setup items
    wrote todo comments and set up for how the switch case will work

--- src/boards/fill-system-board/main.cpp
+++ src/boards/fill-system-board/main.cpp
@@ -1,6 +1,10 @@
 //Credit to Rob Tillaart for the DRV8825 driver
 //https://github.com/RobTillaart/DRV8825
 
+//TODO:
+//Write switch case for doing each valve
+//The switch case will be a letter for each valve
+//Find drivers for
 
 #include "DRV8825.h"
 
@@ -17,6 +21,9 @@ void setup()
   Serial.print("DRV8825_LIB_VERSION: ");
   Serial.println(DRV8825_LIB_VERSION);
 
+  //write code to display letter assignments to user
+  //i.e. type A for motor, C for whatever
+
   stepper.begin(DIRECTION_PIN, STEP_PIN);
 
 }
@@ -24,6 +31,27 @@ void setup()
 
 void loop()
 {
+    int action = 0;
+    switch (action) {
+        case 0: //Stepper Motor, the fuel actuator
+            //whatever
+            break;
+        case 1: //Linear Actuator, P
+            //whatever
+            break;
+        case 3: //Linear Actuator, N
+            //whatever
+            break;
+        case 4: //Logic High, N2O Valve
+            //whatever
+            break;
+        case 5: //Logic High, N2 Valve
+            //whatever
+            break;
+        default:
+            //nothing
+            break;
+    }
 
     stepper.setDirection(DRV8825_CLOCK_WISE);
     stepper.setDirection(DRV8825_COUNTERCLOCK_WISE);
